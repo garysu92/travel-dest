4ffcac656a31559b8012ccdbabdc7ab1e7a37f8c
garysu92 <garysu929292@gmail.com>
2023-08-19 01:25:05 -0400

    add app

--- travel_dest/travel_dest/settings.py
+++ travel_dest/travel_dest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'questionnaire.apps.QuestionnaireConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
